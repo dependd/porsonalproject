4b29b4867861b5d14f64aa79b67361d4c08f14ed
松田幸介 <darby8928@gmail.com>
2018-09-26 22:35:01 +0900

    player追加

---
+++ porsonalproject/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += new Vector3(0,0,0.07f);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.velocity = new Vector3(0, 1, 0);
+        }
+	}
+}
